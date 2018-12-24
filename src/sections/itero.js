094a632a2b587b77f4d21dbad4e65bb9fa6c6297
Sanye Caroline <sanyecg@gmail.com>
2018-12-23 21:22:02 -0300

    create section iTero

---
+++ src/sections/itero.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class iTero extends Component {
+    render() {
+        return (  
+            <div id="iTero">
+            </div>                      
+        );
+    }
+}
+
+export default iTero;
